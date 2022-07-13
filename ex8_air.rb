0a024ae2c9ad1cb04e6c9b2004be4b78bc4fbad3
Oan6 <maelsaby@sfr.fr>
2022-07-13 14:15:56 +0200

    d

--- ex8_air.rb
+++ ex8_air.rb
@@ -2,3 +2,33 @@
 #un nouvel entier tout en gardant la liste triée dans l’ordre croissant. 
 #Le dernier argument est l’élément à ajouter.
 
+
+a = ARGV
+b = a.length
+c = a[0..b-2]
+d = a[b-1]
+
+
+def insert(array, new_element)
+	m = 0
+	for i in 0..array.length
+
+		if new_element.to_i >= array[i].to_i || m == 2 
+			print array[i]
+			print " "
+		else 
+			print new_element
+			print " "
+			m = m + 1
+		end 
+
+		if m == 1 
+			print array[i]
+			print " "
+			m = m+1
+		end 
+
+	end 
+end 
+
+insert(c , d)
