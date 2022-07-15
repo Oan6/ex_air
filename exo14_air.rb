8e66eed088a8f61c836a3dcf469dee691b15b02a
Oan6 <maelsaby@sfr.fr>
2022-07-15 15:22:54 +0200

    d

---
+++ exo14_air.rb
@@ -0,0 +1,3 @@
+#Créez un programme qui vérifie si les exercices de votre épreuve de l’air 
+#sont présents dans le répertoire et qu’ils fonctionnent (sauf celui là).
+#Créez au moins un test pour chaque exercice.
