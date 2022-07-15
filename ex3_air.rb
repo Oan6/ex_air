e2a131a08ab1c7d12616c893834b4b2b16968338
Oan6 <maelsaby@sfr.fr>
2022-07-15 14:46:06 +0200

    s

--- ex3_air.rb
+++ ex3_air.rb
@@ -1,1 +1,17 @@
-Créez un programme qui transforme un tableau de chaînes de caractères en une seule chaîne de caractères. Espacés d’un séparateur donné en dernier argument au programme.
+#Créez un programme qui transforme un tableau de chaînes de caractères
+# en une seule chaîne de caractères. 
+#Espacés d’un séparateur donné en dernier argument au programme.
+
+a = ARGV 
+b = ARGV.length
+c = ARGV[0..b-2]
+d = ARGV[b-1]
+
+def concat(array , separateur)
+	for i in 0..array.length
+		print array[i]
+		print separateur
+	end 
+end 
+
+concat(c ,d)
