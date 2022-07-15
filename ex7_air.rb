357d155d24d0654a5d070103c5ebc1c2c8b3db51
Oan6 <maelsaby@sfr.fr>
2022-07-15 15:16:04 +0200

    downcase

--- ex7_air.rb
+++ ex7_air.rb
@@ -1,2 +1,39 @@
 #Créez un programme qui supprime d’un tableau tous les éléments qui ne contiennent pas 
 #une autre chaîne de caractères.
+
+a = ARGV
+b = ARGV.length
+c = ARGV[0..b-2]
+d = ARGV[b-1]
+
+#probleme avec les maj j'arrive pas a downcase 
+def elimination(array , string)
+	for i in 0..array.length 
+
+		o = array[i]
+		m = 0 
+
+		for j in 0..o.length
+
+			if o[j] == string
+
+				m = m+1
+			end 
+		end
+		if m == 0 
+
+			puts array[i]
+		end  
+	end 
+end 		 
+
+
+
+
+
+
+
+
+
+
+elimination(c,d)
