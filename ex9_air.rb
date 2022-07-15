07296c51e441829025baed66b32f10ca0654ef56
Oan6 <maelsaby@sfr.fr>
2022-07-15 16:16:17 +0200

    d

--- ex9_air.rb
+++ ex9_air.rb
@@ -1,2 +1,38 @@
 #Créez un programme qui fusionne deux listes d’entiers triées en les gardant triées,
 #les deux listes seront séparées par un “fusion”
+
+
+a = ARGV
+
+
+for i in 0..a.length 
+	if a[i] == "fusion"
+		d = i
+	end 
+
+end 
+
+b = a[0..d-1]
+c = a[d+1..a.length]
+puts b 
+puts c 
+
+def fusion(array1 , array2)
+	e = 0
+	r = 0
+	for i in 0..array1.length+array2.length-2
+		if array1[e].to_i <= array2[r].to_i
+			print array1[e]
+			print " "
+			e = e+1 
+		else 
+			print array2[r]
+			print " "
+			r = r+1 
+		end 
+	end
+	print array1[e]
+	print array2[r] 
+end 
+
+fusion(b, c)
